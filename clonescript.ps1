73e5af032932a347bae826e6d6a6b7d8f517ed47
sambireddy26 <sambireddy26@gmail.com>
2018-04-01 23:19:13 +0530

    Create clonescript.ps1

---
+++ clonescript.ps1
@@ -0,0 +1,51 @@
+Param 
+(
+	[string]$Workspace,
+	[string]$BBUserName,
+	[string]$BBPassword,
+	[string]$BaseBranchName
+
+)
+
+
+try  {
+
+    
+	Write-Host Workspace $Workspace
+	Write-Host BBUserName $BBUserName
+	Write-Host BBPassword $BBPassword
+	Write-Host BaseBranchName $BaseBranchName
+	
+        $RepositoryNamesitoryList='sam1', 'sam2'
+
+ 	Foreach ($RepositoryName in $RepositoryNamesitoryList)
+	{
+
+		Write-host "`n====== Repository Name:" $RepositoryName
+		
+		######### Clone Workspace
+	
+			Write-host "--------- Clone Repository:" $RepositoryName
+			cd $Workspace
+            git clone -b $BaseBranchName https://$BBUserName':'$EncodedPassword@github.com/sambireddy26/$RepositoryName.git
+
+            #$command = "git clone https://"+$BBUserName+":"+$BBPassword+"@bitbucket.org/automationanywhere/"+$RepositoryName+".git"
+	    #iex $command
+       Write-Host $LASTEXITCODE
+	     if ($LASTEXITCODE -ne 0)
+           {
+             write-host ************* LASTEXITCODE $LASTEXITCODE
+              write-host $LASTEXITCODE
+                Exit 1
+            }
+
+		}
+		}
+	
+
+		catch
+		{
+			Write-Host ************* $_.Exception.Message 
+			Exit 1
+		}
+
